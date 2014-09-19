c429035e891005e526d64994853b146785b609b0
Andrew McOlash <amcolash@gmail.com>
2014-09-18 23:38:32 -0500

    Generates dynamic svg based on angles

--- Assignment 3/robot.c
+++ Assignment 3/robot.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
   FILE * outFile;
   int angle[4];
   int i;
-  char buffer[4096];
+  char buffer[1024];
 
   /* Check argument length */
   if (argc != 6) {
@@ -33,11 +33,77 @@ int main(int argc, char *argv[]) {
 
   printf("Angles: %i, %i, %i, %i\n", angle[0], angle[1], angle[2], angle[3]);
 
-  fputs("<svg xmlns='http://www.w3.org/2000/svg' height='300px' width='300px'>\n", outFile);
 
 
-  fputs("<rect x='100' y='100' width='100' height='100' fill='red'/>\n", outFile);
+  /* Set up definitions */
+  fputs("<!-- Uses some of the sample code for the robot arm, as provided on the course site -->\n", outFile);
+  fputs("<!-- CS559 Assignment 3 Example - robot arm - by Michael Gleicher -->\n", outFile);
+  fputs("<!-- you may use this example as a starting point for your own work, but course policy requires proper attribution -->\n", outFile);
+  fputs("<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' height='250px' width='400px' viewBox='-50 -50 100 75'>\n", outFile);
+  fputs("<defs>\n", outFile);
+  fputs("  <rect id='segment' x='0' y='-2' height='4' width='10' stroke='lime' fill='cyan'/>\n", outFile);
+  fputs("  <circle id='dot' cx='0' cy='0' r='1' stroke='black' stroke-width='.5' fill='#CCC'/>\n", outFile);
+  fputs("  <rect id='endbox' x='-3' y='-3' height='6' width='6' fill='blue' opacity='.5' stroke='blue'/>\n", outFile);
+  fputs("</defs>\n", outFile);
+
+  /* Set up grid */
+  fputs("<rect x='-50' y='-50' height='75' width='100' stroke='black' fill='#EEE' />\n", outFile);
+  fputs("<line x1='0' y1='-50' x2='0' y2='25' stroke='#888' stroke-width='.5' />\n", outFile);
+  fputs("<line x1='-50' y1='0' x2='50' y2='0' stroke='#888' stroke-width='.5' />\n", outFile);
+
+  /* Actual svg */
+  for (i = 0; i < 4; i++) {
+    if (i == 0) {
+      sprintf(buffer, "<g transform='scale(1,-1)rotate(%i)'>\n", angle[i]);
+      fputs(buffer, outFile);
+    } else {
+      sprintf(buffer, "<g transform='translate(10,0)rotate(%i)'>\n", angle[i]);
+      fputs(buffer, outFile);
+    }
+
+    if (i < 3) {
+      fputs("<use xlink:href='#segment' />\n", outFile);
+    } else {
+      fputs("<use xlink:href='#endbox' />\n", outFile);
+    }
+    fputs("<use xlink:href='#dot' />\n", outFile);
+  }
+
+  for (i=0; i < 4; i++) {
+    fputs("</g>\n", outFile);
+  }
+
   fputs("</svg>", outFile);
+
+  // fputs("  <g transform='translate(10,0) rotate(-45)'>", outFile);
+  // fputs("    <use xlink:href='#segment' />", outFile);
+  // fputs("    <use xlink:href='#dot' />", outFile);
+  // fputs("    <g transform='translate(10,0) rotate(45)'>", outFile);
+  // fputs("      <use xlink:href='#segment' />", outFile);
+  // fputs("      <use xlink:href='#dot' />", outFile);
+  // fputs("      <g transform='translate(10,0) rotate(90)'>", outFile);
+  // fputs("        <use xlink:href='#segment' />", outFile);
+  // fputs("        <use xlink:href='#dot' />", outFile);
+  // fputs("        <g transform='translate(10,0)'>", outFile);
+  // fputs("          <use xlink:href='#dot' />", outFile);
+  // fputs("          <use xlink:href='#redbox' />", outFile);
+  // fputs("        </g>", outFile);
+  // fputs("      </g>", outFile);
+  // fputs("    </g>", outFile);
+  // fputs("  </g>", outFile);
+  // fputs("</g>", outFile);
+  // fputs("</svg>", outFile);
+
+
+
+  // for (i = 0; i < 4; i++) {
+  //   if (i == 0) {
+  //     printf("Got 0\n");
+  //   } else {
+  //   }
+  //   sprintf(buffer, "<rect x='%i' y='%i' width='' height='%i' fill='rgb(%i, %i, %i)' transform='rotate(%i 150 150)'/>\n", angle[i], angle[i], angle[i], angle[i], angle[i], angle[i]);
+  //   //fputs(buffer, outFile);
+  // }
 
   exit(0);
 
