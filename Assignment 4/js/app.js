ff147d98afc4417d1ccaa4095452d7cb62292713
Andrew McOlash <amcolash@gmail.com>
2014-09-26 01:02:02 -0500

    Worked on UI of the page, backing up before going HTML5 route

--- Assignment 4/js/app.js
+++ Assignment 4/js/app.js
@@ -1,3 +1,20 @@
+$(function () {
+  var mySlider = $("input.slider").slider();
+});
+
+$('.slider').slider({
+  formatter: function(value) {
+    return 'Current value: ' + value;
+  }
+});
+
+$('input').change(changeVal);
+
+function changeVal() {
+  alert($('#width').val());
+  alert($('#height').val());
+}
+
 /* Code taken from svg-crowbar, found at https://nytimes.github.io/svg-crowbar */
 function saveSvg() {
   var e = document.createElement('script');
