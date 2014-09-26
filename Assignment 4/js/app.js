9f387b6656200474cc7930187499c84eb19a3496
Andrew McOlash <amcolash@gmail.com>
2014-09-25 23:27:57 -0500

    Updated for a bit of styling, as well as simple svg and save

---
+++ Assignment 4/js/app.js
@@ -0,0 +1,7 @@
+/* Code taken from svg-crowbar, found at https://nytimes.github.io/svg-crowbar */
+function saveSvg() {
+  var e = document.createElement('script');
+  e.setAttribute('src', 'https://nytimes.github.io/svg-crowbar/svg-crowbar.js');
+  e.setAttribute('class', 'svg-crowbar');
+  document.body.appendChild(e);
+}
