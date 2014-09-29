5d6332253836f9b85e440623fad36ea0b46a1c0c
Andrew McOlash <amcolash@gmail.com>
2014-09-29 01:26:55 -0500

    Adding and removing lines, styling changes

--- Assignment 4/app/controllers/index.js
+++ Assignment 4/app/controllers/index.js
@@ -7,12 +7,24 @@ export default Ember.Controller.extend({
 
   tempx1 : 0,
   tempy1 : 0,
+  tempz1 : 0,
   tempx2 : 0,
   tempy2 : 0,
+  tempz2 : 0,
   
-  lines : [[2,3,4,2], [1,2,6,5], [7,1,6,5]],
+  lines : [[2,3,9,0,4,2], [1,-1,-8,2,6,5], [7,1,-1,6,2,5]],
+  finalLines : [[]],
 
   actions: {
+    add : function() {
+      var myThis = this;
+      var lines = this.get('lines');
+      lines.pushObject([ this.get('tempx1'), this.get('tempy1'), this.get('tempz1'), this.get('tempx2'), this.get('tempy2'), this.get('tempz2') ]);
+      this.set('lines', lines);
+      console.log(lines);
+      console.log(this.get('lines'));
+    },
+
     codeGen : function() {
       var svg = $('#svgImage')[0];
       var svg_xml = (new XMLSerializer).serializeToString(svg);
@@ -23,23 +35,30 @@ export default Ember.Controller.extend({
     },
 
     remove : function(element) {
+      var i = 0;
+      var temp = [[]];
       var lines = this.get('lines');
       var index = lines.indexOf(element);
       if (index > -1) {
-        console.log("Removing item " + index + " from the array");
-      }
-      this.set('lines', lines);
-      console.log(this.get('lines'));
+        // Make a copy of the array to manipulate, ember gets grumpy otherwise
+        for (i = 0; i < lines.length; i++) {
+          temp[i] = lines[i];
+        }
+        temp.splice(index, 1);
+      };
+      this.set('lines', temp);
     },
 
     reset : function() {
-      this.set('height', 100);
-      this.set('width', 100);
+      this.set('height', 250);
+      this.set('width', 250);
 
       this.set('tempx1', 0);
       this.set('tempy1', 0);
+      this.set('tempz1', 0);
       this.set('tempx2', 0);
       this.set('tempy2', 0);
+      this.set('tempz2', 0);
     },
 
     save : function() {
