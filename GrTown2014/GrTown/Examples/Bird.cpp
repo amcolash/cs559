bf23187c031689dd6e49e25ca7bff3ad78a076d9
Andrew McOlash <amcolash@gmail.com>
2014-12-03 00:12:19 -0600

    Merged stuff together

--- GrTown2014/GrTown/Examples/Bird.cpp
+++ GrTown2014/GrTown/Examples/Bird.cpp
@@ -12,9 +12,28 @@ void Bird::draw(DrawingState*){
 	glScaled(1, 1, 1.5);
 	glColor4fv(&color.r);
 	GLUquadricObj *quadric = gluNewQuadric();
-	gluCylinder(quadric, .4, .00000, 1, 100, 100);
+	gluCylinder(quadric, .2, 0, 1, 20, 20);
 	gluQuadricDrawStyle(quadric, GLU_FILL);
 	gluSphere(quadric, 0.5, 36, 18);
 	gluDeleteQuadric(quadric);
+	quadric = gluNewQuadric();
+	glTranslated(0, 0, -1.1);
+	gluSphere(quadric, .7, 20, 20);
+	glPopMatrix();
+	glPushMatrix();
+	//glScaled(1, 1, 1.5);
+	glTranslated(0, 0, -3);
+	quadric = gluNewQuadric();
+	gluCylinder(quadric, .05, .05, 1, 20, 20);
+	glRotated(20, 0, 1, 1);
+	glTranslated(-.2, 0, 0);
+	gluCylinder(quadric, 0.05, .05, 1, 20, 20);
+	glTranslated(.2, 0, 0);
+	glRotated(-40, 0, 1, 1);
+	glTranslated(.2, 0, 0);
+	gluCylinder(quadric, 0.05, 0.05, 1, 10, 10);
+	glPopMatrix();
+	glPushMatrix();
+
 	glPopMatrix();
 }
