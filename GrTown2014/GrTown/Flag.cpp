904b75b1a37e63fc198dffe26c5e63f62dfbeb1d
Jason Bernstein <jbernstein30@gmail.com>
2014-12-10 18:52:16 -0600

    Shader waves flag

--- GrTown2014/GrTown/Flag.cpp
+++ GrTown2014/GrTown/Flag.cpp
@@ -5,6 +5,7 @@
 
 #include "Utilities/ShaderTools.H"
 
+GLfloat waveTime = 0.0;
 float count = 0.0f;
 
 Flag::Flag(float x, float y, float z, float r, float g, float b) :
@@ -14,36 +15,45 @@ color(r, g, b), shader(shader)
 }
 void Flag::draw(DrawingState* ds){
 	
-	count += ds->speedup*.1;
+	count += ds->speedup*.001;
 	if (!triedShader) {
 		triedShader = true;
 		char* error;
-		if (!(shader = loadShader("flag.vert", "flag.frag", error))) {
+		if (!(shader = loadShader("Flag.vert", "Flag.frag", error))) {
 			std::string s = "Can't Load Surface Shader:";
 			s += error;
 			fl_alert(s.c_str());
 		}
 	}
+	
 	if (shader != 0) {
 		glUseProgram(shader);
-		GLint timeUniformLocation = glGetUniformLocation(shader, "_time");
+		GLint timeUniformLocation = glGetUniformLocation(shader, "count");
 		glUniform1i(timeUniformLocation, count);
-		GLfloat UniformMatrix = glGetUniformLocation(shader, "_mvProj");
-		glUniform1i(UniformMatrix, GL_MODELVIEW_PROJECTION_NV);
+		GLfloat waveWidth = 0.1, waveHeight = 10.3;
+		GLint waveTimeLoc = glGetUniformLocation(shader, "waveTime");
+		GLint waveWidthLoc = glGetUniformLocation(shader, "waveWidth");
+		GLint waveHeightLoc = glGetUniformLocation(shader, "waveHeight");
+		glUniform1f(waveTimeLoc, waveTime);
+		glUniform1f(waveWidthLoc, waveWidth);
+		glUniform1f(waveHeightLoc, waveHeight);
+		//GLfloat UniformMatrix = glGetUniformLocation(shader, "_mvProj");
+		//glUniform1i(UniformMatrix, GL_MODELVIEW_PROJECTION_NV);
 	}
 
-		
+
+	/* Draw flag */
 
 		glPushMatrix();
 		glTranslated(0, 90, 0);
-
+		glRotated(90, 0, 1, 0);
 		glBegin(GL_QUADS);
 		glVertex3f(0, -20, 0);
 		glVertex3f(0, 10, 0);
 		glVertex3f(30, 10, 30);
 		glVertex3f(30, -20, 30);
 		glEnd();
-
+		
 		glBegin(GL_QUADS);
 		glVertex3f(30, -20, 30);
 		glVertex3f(30, 10, 30);
@@ -54,11 +64,13 @@ void Flag::draw(DrawingState* ds){
 		glPopMatrix();
 		glUseProgram(0);
 
+		/* Draw flagpole */
 		glPushMatrix();
 		GLUquadricObj *quadric = gluNewQuadric();
 		glColor4fv(&color.r);
 		glRotated(-90, 1, 0, 0);
 		gluCylinder(quadric, 2, 2, 100, 10, 10);
 		glPopMatrix();
+		waveTime += .1;
 	
 }
--- GrTown2014/GrTown/Shaders/flag.frag
+++ GrTown2014/GrTown/Shaders/flag.frag
@@ -1,30 +1,17 @@
-precision highp float;
+varying vec3 fPosition;
+varying vec3 fNormal;
+varying vec3 uPos;
 
-varying vec3 n;
-varying vec2 uv;
+void main()
+{
+  float ys = uPos.y*5.0 - floor(uPos.y*5.0);
+  float xs = uPos.z*5.0 - floor(uPos.z*5.0);
 
-//uniform sampler2D tex;
-
-//#pragma include "noise2D.glsl // for snoise(vec2 v)
-//#pragma include "noise3D.glsl" //  for snoise(vec3 v)
-//#pragma include "noise4D.glsl" //  for snoise(vec4 v)
-//#pragma include "cellular2D.glsl" //  for cellular(vec2 P)
-//#pragma include "cellular2x2.glsl" //  for cellular2x2(vec2 P)
-//#pragma include "cellular2x2x2.glsl" //  for cellular2x2x2(vec3 P)
-//#pragma include "cellular3D.glsl" //  cellular(vec3 P)
-
-void main(void)
-{/*
-	float ys = n.y*5.0 - floor(n.y*5.0);
-	  float y = smoothstep(.45,.55,ys);
-  float x = smoothstep(.55, .45, ys);
-  float x1 = n.x + pow(n.x,n.x);
-	float y1 = n.y + pow(n.y,n.y);
-	float x2 = step(x1, 0.0);
-	float y2 = smoothstep(x2, x1, y1);
-	asdf
-    vec3 eyeSpaceLightDirection = vec3(2.0,1.0,1.0);
-    //float diffuse = max(0.0,dot(n,eyeSpaceLightDirection));
-	//gl_FragColor = vec4(n.xz*diffuse, x2, y2);*/
-	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  float xc = smoothstep(.45,.55,xs);
+  float yc = step(.45, ys);
+  
+  //vec3 n = normalize(fNormal);
+  //float dl = max(0.0, dot(n, vec3(.5,.707,0)));
+  
+  gl_FragColor = vec4(xc, xc, 0.0, 5)  ;
 }
